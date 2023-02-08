752a7d4664ba69df66244f4416aea99d3ef1cf21
donghun93 <alswn4516@gmail.com>
2023-02-08 14:12:29 +0900

    Feat: add log

--- src/main/java/com/devwinter/jenkinscicd/controller/HelloApiController.java
+++ src/main/java/com/devwinter/jenkinscicd/controller/HelloApiController.java
@@ -1,9 +1,11 @@
 package com.devwinter.jenkinscicd.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/jenkins")
 public class HelloApiController {
@@ -11,6 +13,7 @@ public class HelloApiController {
     @GetMapping
     public String hello() {
         // test hello
+        log.info("hello");
         return "hello";
     }
 }
